b5d2c8752167afb0e2910363022ccf2c48e7d80d
Jonathan Pilling <jonathanpilling77@gmail.com>
2019-03-25 14:04:32 -0600

    MATLAB HW3 part a

---
+++ CS_3200/HW3/lup.m
@@ -0,0 +1,31 @@
+function [L, U, P] = lup(A)
+
+N = size(A);
+U = A;
+L = eyes(N);
+P = eyes(N);
+
+for i=1:N-1
+    [~, m] = abs(max(U))
+
+    if(m>= i)
+        //Swap rows U(i,i : N) and U(m,i : N).
+        temp = U(i,i:N);
+        U(i,i:N) = U(m(i),i:N);
+        U(m(i),i:N) = temp;
+
+        //Swap rows L(i, 1 : i − 1) and L(m, 1 : i − 1).
+        temp2 = L(i, 1: i-1);
+        L(i, 1:i-1) = L(m(i), 1:i-1);
+        L(m(i), 1:i-1) = temp2;
+
+        //Swap row m of P and i of P
+        P([m,i],:) = P([i,m],:);
+       
+        
+    end
+    for j=(i+1):N
+            L(j,i) = U(j,i)/U(i,i);
+            U(j,i:N) = U(j, i:N) - (L(j,i)*U(i,i:N))
+    end
+end
