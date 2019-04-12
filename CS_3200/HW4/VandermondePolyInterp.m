c114e8c1382a326247bc3aa964cc1a61501774f4
Jonathan Pilling <jonathanpilling77@gmail.com>
2019-04-12 14:45:22 -0600

    3200 done!

--- CS_3200/HW4/VandermondePolyInterp.m
+++ CS_3200/HW4/VandermondePolyInterp.m
@@ -5,41 +5,53 @@ clc;
 f = @(x) sin(x) + 0.1*rand(length(x),1);
 
 %% Numbers of data points.
-Nd = 5;
+Nd = 40; % Change this value
 
-%% Setup the points at which we want to evaluate the interpolant
-xe = linspace(-1,1,1000)'; %generate evenly-spaced points
-ye = f(xe); %evaluate function
+for i=1:3
 
-%% Start plotting stuff
-figure
-plot(xe,ye,'black');
-%pause %waits for input
+    %% User defined l value
+    l = i; %This will give 1,2
+    if i == 3 %This will give Nd-1
+        l = Nd - 1;
+    end
+    
+    %% Setup the points at which we want to evaluate the interpolant
+    xe = linspace(-1,1,10000)'; %generate evenly-spaced points
+    ye = f(xe); %evaluate function
 
-x = linspace(-1,1,Nd)'; %generate evenly-spaced points in [-1,1].
-y = f(x);
+    %% Start plotting stuff
+    figure
+    plot(xe,ye,'black');
+    %pause %waits for input
 
-%% Form Vandermonde matrix "intelligently"
-V = zeros(Nd,Nd);
-for vit = 1:Nd
-    V(:,vit) = x.^(vit-1);
+    x = linspace(-1,1,Nd)'; %generate evenly-spaced points in [-1,1].
+    y = f(x);
+
+    %% Form Vandermonde matrix "intelligently"
+    V = zeros(Nd,Nd);
+    for vit = 1:Nd
+        V(:,vit) = x.^(vit-1);
+    end
+
+    %% Compute coefficients by QR decomposition.
+    [Q,R] = qr(V); % Computing coefficients by QR
+    display(Q)
+    display(R)
+    %a = V\y;
+    a = R \ (Q' * y);
+    %% Evaluate polynomial interpolant by building eval matrix and
+    %% multiplying with coefficients.
+    Ve = zeros(length(xe),Nd);
+    for vit = 1:l 
+        Ve(:,vit) = xe.^(vit-1);
+    end
+    ye_num = Ve*a; %Do the matrix-vector product to predict values
+
+    %% Plot stuff
+    hold on;
+    plot(xe,ye_num,'-');
+    drawnow
 end
-
-%% Compute coefficients by QR decomposition.
-a = V\y;
-
-%% Evaluate polynomial interpolant by building eval matrix and
-%% multiplying with coefficients.
-Ve = zeros(length(xe),Nd);
-for vit = 1:Nd
-    Ve(:,vit) = xe.^(vit-1);
-end
-ye_num = Ve*a; %Do the matrix-vector product to predict values
-
-%% Plot stuff
-hold on;
-plot(xe,ye_num,'-');
-drawnow
 %pause
 
 
