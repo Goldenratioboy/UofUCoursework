9ac158fd52df3729b2cbd9c0bf30c3223d72adf7
Jonathan Pilling <jonathanpilling77@gmail.com>
2019-09-15 17:22:36 -0600

    n algo broken

--- CS4150/galaxyQuest.py
+++ CS4150/galaxyQuest.py
@@ -6,8 +6,59 @@ def distance(p1, p2, d):
     else:
         return False
 
+def distanceTest(x, y, d):
+    if x + y <= d:
+        return True
+    else:
+        return False
 
-starList = set()
+# A is an array of stars, (x,y) coordinates
+def StarCounter(A, d):
+    print(A)
+    print(d)
+    if len(A) is 0:
+        return None    
+    elif len(A) is 1:
+        return A
+    else:
+        aPrime = []
+        y = []
+
+        #Finding A' array of stars and y value?
+        for p1, p2, p3 in zip(*[iter(A)]*3):
+            if distance(p1, p2, d) is True and distance(p1, p3, d) is True:
+                aPrime.append(p1)
+        if len(A) % 3 == 1 or len(A) % 3 == 2:
+            y.append(A[-1])
+
+        x = StarCounter(aPrime, d) # call again with smaller list
+        
+        if x is None:
+            yCount = 0
+            if len(A) % 3 != 0:
+                for star in A:
+                    if distance(star, y, d) is True:
+                        yCount += 1              
+                if yCount >= 2:
+                    return y                
+                else:
+                    return None
+
+            else:
+                return None
+
+                
+        else:
+            xCount = 0
+            for i in A:
+                if distance(i, x, d) is True:
+                    xCount += 1
+            if xCount >= 2:
+                return x
+            else:
+                return None
+
+starList = list()
 
 #Read first line to get params
 params = sys.stdin.readline().split()
@@ -17,25 +68,23 @@ k = int(params[1]) #Number of stars to read
 #Create our list of stars
 for i in sys.stdin:
     params = i.split()
-    starList.add((int(params[0]), int(params[1])))
+    starList.append((int(params[0]), int(params[1])))
 
 d2 = d**2
 
-highestStarCount = 0
+finalList = StarCounter(starList, d2)
 
-for star in starList:
-    count = 1 # Must count this first star if we find other stars
-    for star2 in starList:
-        if star is star2:
-            break
-        if distance(star, star2, d2) is True:
-            count += 1
+print('Final List:' + finalList)
 
-    if count > highestStarCount:
-        highestStarCount = count
-
-
-if highestStarCount > k/2:
-    print(highestStarCount)
+if finalList is None:
+    print('NO')
 else:
-    print('NO')    
+    count = 0
+
+    for star in starList:
+        if distance(finalList[0], star, d) is True:
+            count += 1
+    if count > k/2:
+        print(count)
+    else:
+        print('NO')
