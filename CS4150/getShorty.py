0a76ba0237a6c46e4e4e7fdca193dbd4f3a93ad1
Jonathan Pilling <jonathanpilling77@gmail.com>
2019-10-02 23:33:27 -0600

    Time out error, but computations might be correct?

--- CS4150/getShorty.py
+++ CS4150/getShorty.py
@@ -4,8 +4,6 @@ import heapq as q
 
 from collections import defaultdict
 
-inDungeon = True
-
 #graph class to represent graph
 class graph:
 
@@ -24,43 +22,45 @@ class graph:
             dist.append(-1)
             prev.append(None)
 
-        dist[sVertex] = 0
+        dist[sVertex] = 1
 
         pq = []
         q.heappush(pq, (sVertex, 0))
 
         while len(pq) > 0:
             u = q.heappop(pq)
-            for u,v,w in G.graph:
-                if dist[v] < dist[u] * w:
-                    dist[v] = dist[u] * w
+            for u,v in E.graph.keys():
+                if dist[v] < dist[u] * E.graph[u,v]:
+                    dist[v] = dist[u] * E.graph[u,v]
                     prev[v] = u
-                    q.heappush(pq, (v, dist[v]))
+                    q.heappush(pq, (v, dist[u]))
 
         return dist, prev
         
+inDungeon = True
 
 while(inDungeon):
     params = sys.stdin.readline().split()
 
-    n, m = params[0], params[1]
-
-    vList = [None] * n # list of vertices
-
-    G = Graph(n)
+    n, m = int(params[0]), int(params[1])
 
     if m is 0 and n is 0: # break out if we reach EOF
         inDungeon = False
+        break
+
+    vList = [None] * n # list of vertices
+
+    G = graph(n)
 
     for i in range(0, m):
         params = sys.stdin.readline().split()
-
         # Add vertices with weight of dmg
-        G.addEdge(params[0], params[1], params[2])
+        G.addEdge(int(params[0]), int(params[1]), float(params[2]))
 
     dist, prev = G.Dijkstras(0, vList, G)
-    print(dist[n])
-
-
+    if dist[n-1] is -1:
+        print( "{:.{}f}".format( 1 , 4 ) )
+    else:
+        print( "{:.{}f}".format( dist[n-1], 4 ) )
 
     
